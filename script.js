720eedc6b6bab8cd7b63bbc15420ef846a55db88
taskeenmulla54-design <taskeenmulla54@gmail.com>
2026-04-17 18:52:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let wishlist = 0;
 function addToCart(){
 cart++;
 document.getElementById("cart-count").innerText = cart;
-alert("Added to Cart");
 }
 
 function addToWishlist(){
 wishlist++;
 document.getElementById("wish-count").innerText = wishlist;
-alert("Added to Wishlist");
 }
