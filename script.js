b413725d02dd65bd5e1c30d0a0c861c847a3ddc2
taskeenmulla54-design <taskeenmulla54@gmail.com>
2026-04-17 18:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Welcome to ShopMe - Created by Taskeen");
+let cart = 0;
+let wishlist = 0;
+
+function addToCart(){
+cart++;
+document.getElementById("cart-count").innerText = cart;
+alert("Added to Cart");
+}
+
+function addToWishlist(){
+wishlist++;
+document.getElementById("wish-count").innerText = wishlist;
+alert("Added to Wishlist");
+}
