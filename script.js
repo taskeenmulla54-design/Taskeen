3a3f70ddd67c4fc8ab3788dd2f52cbb35e45bb04
taskeenmulla54-design <taskeenmulla54@gmail.com>
2026-04-17 19:27:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-alert("Welcome to ShopMe");
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+function addToCart(name,price){
+
+cart.push({name,price})
+
+localStorage.setItem("cart",JSON.stringify(cart))
+
+alert("Added to cart")
+
+}
+
+function addToWishlist(name){
+
+wishlist.push(name)
+
+localStorage.setItem("wishlist",JSON.stringify(wishlist))
+
+alert("Added to wishlist")
+
+}
+
+function displayCart(){
+
+let cartItems = document.getElementById("cart-items")
+
+if(!cartItems) return;
+
+cartItems.innerHTML = ""
+
+cart.forEach(item=>{
+cartItems.innerHTML += `
+<p>${item.name} - ₹${item.price}</p>
+`
+})
+
+}
+
+function displayWishlist(){
+
+let wishlistItems = document.getElementById("wishlist-items")
+
+if(!wishlistItems) return;
+
+wishlistItems.innerHTML = ""
+
+wishlist.forEach(item=>{
+wishlistItems.innerHTML += `
+<p>${item}</p>
+`
+})
+
+}
+
+function goToPayment(){
+window.location.href="payment.html"
+}
+
+displayCart()
+displayWishlist()
